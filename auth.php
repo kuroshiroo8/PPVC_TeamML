5449011155a7d5aef2edb3997d1e7c9565dcafb5
Irving Velica <irving.velica@uabc.edu.mx>
2020-10-20 23:40:34 -0700

    Auth
    ahora bien hecho

--- auth.php
+++ auth.php
@@ -12,10 +12,10 @@ $client_id = '4363478170680107'; // Your client_id
 $client_secret = 'pH84I1Ko0p7pddgglsH4XzRlXYUXl6ty'; // Your client_secret
 $redirect_uri = 'https://ppvcteamml.com/'; // Your redirect_uri
 $code = 'TG-5f8f9c20967c430006a6919d-659993463'; // The parameter CODE
-$refresh_token = 'refresh_token_example'; // Your refresh_token
+//$refresh_token = 'refresh_token_example'; // Your refresh_token
 
 try {
-    $result = $apiInstance->getToken($grant_type, $client_id, $client_secret, $redirect_uri, $code, $refresh_token);
+    $result = $apiInstance->getToken($grant_type, $client_id, $client_secret, $redirect_uri, $code, '');
     print_r($result);
 } catch (Exception $e) {
     echo 'Exception when calling OAuth20Api->getToken: ', $e->getMessage(), PHP_EOL;
